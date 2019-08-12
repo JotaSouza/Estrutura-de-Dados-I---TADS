f4ac017a10d8f1acb16205bd7ffbdf30f2269234
unknown <cge.sede@IFC-CGE03.IFRSL.local>
2019-08-12 13:30:48 -0300

    Create MainDatas.java

---
+++ MainDatas.java
@@ -0,0 +1,40 @@
+package ExAULA3;
+
+import java.util.Scanner;
+
+public class MainDatas {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		FunçãoDatas veriDatas = new FunçãoDatas();
+		
+		int dia, mes, ano = 0;
+		String data = "";
+		System.out.println("::.. Funções para verificar datas ..::");
+		System.out.print("\nInforme o dia: ");
+		dia = entrada.nextInt();
+		System.out.print("Informe o mês. (1 - 12): ");
+		mes = entrada.nextInt();
+		System.out.print("Informe o ano com 4 dígitos: ");
+		ano = entrada.nextInt();
+		
+		System.out.println("\nVerificando ...\n");
+		veriDatas.diaValido(dia, mes);
+		veriDatas.mesValido(mes);
+		veriDatas.anoBissexto(ano);
+		
+		System.out.print("\nPor favor, informe a data novamente neste formato (DD/MM/AAAA): ");
+		data = entrada.next();
+		veriDatas.dataValida(data);
+		
+		System.out.println("\nEscrevendo a data informada por extenso:\n");
+		veriDatas.escreveData(dia, mes, ano);
+		
+		
+		entrada.close();
+	
+
+	}
+
+}
